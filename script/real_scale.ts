9ba808e0df9319376f47ae2eeb271f48918b5298
npx4x4 <mickgame1881@gmail.com>
2026-01-10 22:01:48 +0900

    実寸シミュレータのhtml部分作業中1

--- script/real_scale.ts
+++ script/real_scale.ts
@@ -1,1 +1,12 @@
+export{}
 console.log("Hello World");
+
+// 画面サイズ(画素数を取得)
+let scr_w: number = screen.width;
+let scr_h: number = screen.height;
+
+//html要素を取得
+const setButton = document.getElementById('')
+
+function
+
