ce339e5e84f233a8cc21e61d0e61c703c5c7328f
npx4x4 <mickgame1881@gmail.com>
2026-01-12 19:34:09 +0900

    real_scaleのメニュー->シミュレーション画面の推移組み

--- script/real_scale.js
+++ script/real_scale.js
@@ -4,7 +4,14 @@ console.log("Hello World");
 let scr_w = screen.width;
 let scr_h = screen.height;
 //html要素を取得
+let ele = document.documentElement;
 const showResolution = document.getElementById('show_resolution');
-const setButton = document.getElementById('');
-showResolution.innerText = "デバイスの解像度: " + String(scr_w) + " x " + String(scr_h);
+const setButton = document.getElementById('rs_set');
+const simGround = document.getElementById('sim_ground');
+showResolution.innerText = `デバイスの解像度: ${scr_w} x  ${scr_h}`;
+simGround.style.display = "none";
+function rs_start() {
+    simGround.style.display = "block";
+}
+setButton.addEventListener('click', rs_start);
 //# sourceMappingURL=real_scale.js.map
--- script/real_scale.ts
+++ script/real_scale.ts
@@ -6,9 +6,17 @@ let scr_w: number = screen.width;
 let scr_h: number = screen.height;
 
 //html要素を取得
+let ele = document.documentElement;
 const showResolution = document.getElementById('show_resolution') as HTMLElement;
-const setButton = document.getElementById('')
+const setButton = document.getElementById('rs_set') as HTMLElement;
+const simGround = document.getElementById('sim_ground') as HTMLElement;
 
-showResolution.innerText = "デバイスの解像度: " + String(scr_w) + " x " + String(scr_h);
+showResolution.innerText = `デバイスの解像度: ${scr_w} x  ${scr_h}`;
+simGround.style.display = "none";
 
+function rs_start() {
+    simGround.style.display = "block";
 
+}
+
+setButton.addEventListener('click', rs_start);
