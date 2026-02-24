16842c92c1cea0dd96251f8ba28da4ff84aa927b
npx4x4 <mickgame1881@gmail.com>
2026-02-24 11:58:01 +0900

    更新

--- 03_use-react/src/App.tsx
+++ 03_use-react/src/App.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { get } from 'http';
 
 function App() {
-  const [rows, setRows] = React.useState(6);
-  const [cols, setCols] = React.useState(7);
-
+  // 席の初期設定
+  const [rows, setRows] = React.useState(7);
+  const [cols, setCols] = React.useState(6);
+  // const add_row = get
+  const seatingChart = [...Array(rows).map(() => Array(cols).fill("机"))]
   return (
     <div className="App">
-      <div class
+      <div className="tool_head">
+        <a className="back_home" href="/">
+          <img className="back_home_img" src="/img/esc.png"/>
+          ホームへ戻る
+        </a>
+        <h1 className="tool_title">席替えスロットル</h1>
+      </div>
+      <div className="table_area">
+        <div className="table_rows">
+          {soyBeans.map((_, index) => (
+          <div key={index} style={{ fontSize: '10px' }}>あ</div>
+        ))}
+        </div>
+      </div>
     </div>
   );
 }
