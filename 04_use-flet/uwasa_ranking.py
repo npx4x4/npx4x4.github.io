86c126acc22454999a7d428c3c47383baf32afb6
npx4x4 <mickgame1881@gmail.com>
2026-03-14 23:33:32 +0900

    flet動作テスト

---
+++ 04_use-flet/uwasa_ranking.py
@@ -0,0 +1,24 @@
+import flet as ft
+
+def main(page: ft.Page):
+    page.title = "噂ランキング"
+    page.bgcolor = ft.Colors.WHITE
+    
+    url_launcher = ft.UrlLauncher()
+
+    async def go_home(e: ft.Event[ft.Button]):
+        #"_self" <- タブ遷移せずurlを開く
+        await url_launcher.launch_url("https://npx4x4-tools.vercel.app/", web_only_window_name="_self",)
+    
+    home_button = ft.Button(content="ホームに戻る", on_click=go_home,)
+    title = ft.Text("噂ランキング", theme_style=ft.TextThemeStyle.DISPLAY_LARGE)
+
+
+    page.add(
+        ft.Row(
+            home_button,
+            title,
+        )
+    )
+
+ft.run(main)
